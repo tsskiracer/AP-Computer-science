cd2c0e3ac1efbc4990717811bc07ea9dca27d8a7
tsskiracer <William.Starkey@MU-C1-03.mullen.pvt.k12.co.us>
2015-08-27 08:36:02 -0600

    Modulus quizzer finished

--- AP computer science/src/ModulusQuizzerProject.java
+++ AP computer science/src/ModulusQuizzerProject.java
@@ -2,19 +2,61 @@ import java.util.Scanner;
 public class ModulusQuizzerProject
 	{
 
-		public static void main(String[] args)
-			{
-				
-				System.out.println("Hello! How many questions would you like between 1 and 10?");
-				Scanner userInput = new Scanner(System.in);
-				
-				int ques = userInput.nextInt();
-				
-				for (ques){
-					randomInt = a; 
-					RandomInt = b;
-				
-				
-			}		
-
+		public static void main(String[] args) {
+		{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Hello! How many questions would you like?");				
+		int ques = userInput.nextInt();
+		int answerCount = 0;
+		for (int i=0; i<ques; i++ ){
+			int x = ((int)(Math.random()*10)+11);
+			int y = ((int)(Math.random()*10)+1);
+			int answer;
+			if (i==0)
+				{
+					System.out.println(x + "%" + x + "= ");
+					answer = userInput.nextInt();
+					if (answer == x%x)
+						{
+							System.out.println("Correct!");
+							answerCount++;
+						}
+					else
+						{
+							System.out.println("Incorrect");
+						}
+				}
+			else if(i==1)
+				{
+					System.out.println(y + "%" + x + "=" );
+					answer = userInput.nextInt();
+					if (answer == y%x)
+						{
+							System.out.println("Correct!");
+							answerCount++;
+						}
+					else
+						{
+							System.out.println("Incorrect");
+							answerCount++;
+						}
+				}
+			else
+				{ 
+					System.out.println(x + "%" + y + "=");
+					answer = userInput.nextInt();					
+					if (answer == x%y)
+						{
+							System.out.println ("Correct!");
+							answerCount++;
+						}
+					else 
+						{
+							System.out.println ("Incorrect");
+						}
+				}
+		}
+		System.out.println("You got " +answerCount+ " correct.");
+	}
+}
 	}
